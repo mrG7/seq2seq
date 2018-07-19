ad55ddf0451524f993751a7f71e72a839197e230
Michail Tarasiou <mat10@point59.doc.ic.ac.uk>
2018-07-19 12:23:43 +0100

    added functionality for visual feature pretrain

--- models.py
+++ models.py
@@ -92,9 +92,7 @@ class VisualFeaturePretrainModel(BasicModel):
 
         if self.options['mode'] == 'train':
             self.train_era_step = self.options['train_era_step']
-            self.encoder_inputs, self.target_labels, self.decoder_inputs, self.encoder_inputs_lengths, \
-            self.target_labels_lengths, self.decoder_inputs_lengths, self.max_input_len = \
-                get_training_data_batch(self.data_paths, self.options)
+            self.encoder_inputs, self.target_labels = get_lrw_training_data_batch(self.data_paths, self.options)
             if self.options['num_decay_steps'] is not None:
                 self.num_decay_steps = self.options['num_decay_steps']
             else:
@@ -106,11 +104,9 @@ class VisualFeaturePretrainModel(BasicModel):
             self.build_train_graph()
 
         elif self.options['mode'] == 'test':
-            self.encoder_inputs, self.target_labels, self.decoder_inputs, self.encoder_inputs_lengths, \
-            self.target_labels_lengths, self.decoder_inputs_lengths, self.max_input_len = \
-                get_inference_data_batch(self.data_paths, self.options)
-            self.max_decoding_steps = tf.to_int32(
-                tf.round(self.options['max_out_len_multiplier'] * tf.to_float(self.max_input_len)))
+            self.encoder_inputs, self.target_labels = get_lrw_inference_data_batch(self.data_paths, self.options)
+            # self.max_decoding_steps = tf.to_int32(
+            #     tf.round(self.options['max_out_len_multiplier'] * tf.to_float(self.max_input_len)))
             self.build_inference_graph()
 
         else:
@@ -156,100 +152,80 @@ class VisualFeaturePretrainModel(BasicModel):
         with tf.variable_scope('temporal_conv'):
             self.logits = conv_backend(self.features_res, self.options)
 
-        # if self.options['save_summaries']:
-        #     tf.summary.scalar('features', tf.reduce_max(self.features_res))
+        with tf.variable_scope('train_metrics'):
+            self.train_loss = tf.reduce_mean(
+                tf.losses.softmax_cross_entropy(self.target_labels, self.logits))
+            correct_prediction = tf.equal(tf.argmax(self.logits, 1),
+                                          tf.argmax(self.train_labels, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            if self.options['save_summaries']:
+                tf.summary.scalar('loss', self.train_loss)
+                tf.summary.scalar('accuracy', self.accuracy)
 
-    #     with tf.variable_scope('temp_conv_backend'):
-    #         pass
-    #
-    #
-    #
-    #     with tf.variable_scope('loss_function'):
-    #         self.train_loss = tf.contrib.seq2seq.sequence_loss(
-    #             decoder_outputs, self.target_labels, weights=target_weights)
-    #         if self.options['save_summaries']:
-    #             tf.summary.scalar('cross_entropy', self.train_loss)
-    #
-    #     with tf.variable_scope('training_parameters'):
-    #         params = tf.trainable_variables()
-    #         # clip by gradients
-    #         max_gradient_norm = tf.constant(10., dtype=tf.float32, name='max_gradient_norm')
-    #         self.gradients = tf.gradients(self.train_loss, params)
-    #         clipped_gradients, _ = tf.clip_by_global_norm(self.gradients, max_gradient_norm)
-    #
-    #         # Optimization
-    #         self.global_step = tf.Variable(0, trainable=False)
-    #         self.increment_global_step = tf.assign(self.global_step, self.global_step + 1)
-    #         initial_learn_rate = tf.constant(self.options['learn_rate'], tf.float32)
-    #         learn_rate = tf.train.exponential_decay(learning_rate=initial_learn_rate, global_step=self.global_step,
-    #                                                 decay_steps=self.num_decay_steps, decay_rate=self.options['decay_rate'],
-    #                                                 staircase=True)
-    #         # self.optimizer = tf.train.MomentumOptimizer(learn_rate, momentum=0.9)#.minimize(cross_entropy)
-    #         # learn_rate = tf.constant(self.options['learn_rate'], tf.float32)
-    #         self.optimizer = tf.train.AdamOptimizer(learn_rate)
-    #
-    #         self.update_step = self.optimizer.apply_gradients(zip(clipped_gradients, params), global_step=self.global_step)
-    #         self.accuracy, self.accuracy2 = char_accuracy(self.target_labels, decoder_greedy_pred, self.target_labels_lengths)
-    #
-    #         if self.options['save_summaries']:
-    #             tf.summary.scalar('accuracy', self.accuracy2)
-    #
-    # def train(self, sess, number_of_steps=None, reset_global_step=False):
-    #
-    #     # update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    #     # with tf.control_dependencies(update_ops):
-    #     #     train_op = self.optimizer.minimize(self.train_loss)
-    #
-    #     if number_of_steps is not None:
-    #         assert type(number_of_steps) is int
-    #         start_epoch = 0
-    #         num_epochs = 1
-    #     else:
-    #         number_of_steps = self.number_of_steps_per_epoch
-    #         start_epoch = self.options['start_epoch']
-    #         num_epochs = self.options['num_epochs']
-    #
-    #     if self.options['restore']:
-    #         self.restore_model(sess)
-    #
-    #     if reset_global_step:
-    #         initial_global_step = self.global_step.assign(0)
-    #         sess.run(initial_global_step)
-    #
-    #     for epoch in range(start_epoch, start_epoch + num_epochs):
-    #
-    #         for step in range(number_of_steps):
-    #
-    #             _, _, loss, acc, acc2, lr, sp, summ, gs = sess.run(
-    #                 [self.update_step,
-    #                  self.increment_global_step,
-    #                  self.train_loss,
-    #                  self.accuracy,
-    #                  self.accuracy2,
-    #                  self.optimizer._lr_t,  # _learning_rate,  # .optimizer._lr_t,
-    #                  self.sampling_prob,
-    #                  self.merged_summaries,
-    #                  self.global_step])
-    #             print("%d,%d,%d,%d,%.4f,%.4f,%.4f,%.8f,%.4f"
-    #                   % (epoch,
-    #                      self.options['num_epochs'],
-    #                      step,
-    #                      self.number_of_steps_per_epoch,
-    #                      loss, acc, acc2, lr, sp))
-    #
-    #             if self.options['save_summaries']:
-    #                 self.writer.add_summary(summ, gs)
-    #
-    #             if (self.train_era_step % self.options['save_steps'] == 0) and self.options['save']:
-    #                 # print("saving model at global step %d..." % global_step)
-    #                 self.save_model(sess=sess, save_path=self.options['save_model'] + "_epoch%d_step%d" % (epoch, step))
-    #                 # print("model saved.")
-    #
-    #             self.train_era_step += 1
-    #
-    #     # save before closing
-    #     if self.options['save']:
-    #         self.save_model(sess=sess, save_path=self.options['save_model'] + "_final")
+        with tf.variable_scope('optimizer'):
+            params = tf.trainable_variables()
+            self.gradients = tf.gradients(self.train_loss, params)
+
+            initial_learn_rate = tf.constant(self.options['learn_rate'], tf.float32)
+            learn_rate = tf.train.exponential_decay(learning_rate=initial_learn_rate, global_step=self.global_step,
+                                                    decay_steps=self.num_decay_steps, decay_rate=self.options['decay_rate'],
+                                                    staircase=True)
+            self.optimizer = tf.train.AdamOptimizer(learn_rate)
+            self.update_step = self.optimizer.apply_gradients(zip(self.gradients, params), global_step=self.global_step)
+
+    def train(self, sess, number_of_steps=None, reset_global_step=False):
+
+        # update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        # with tf.control_dependencies(update_ops):
+        #     train_op = self.optimizer.minimize(self.train_loss)
+
+        if number_of_steps is not None:
+            assert type(number_of_steps) is int
+            start_epoch = 0
+            num_epochs = 1
+        else:
+            number_of_steps = self.number_of_steps_per_epoch
+            start_epoch = self.options['start_epoch']
+            num_epochs = self.options['num_epochs']
+
+        if self.options['restore']:
+            self.restore_model(sess)
+
+        if reset_global_step:
+            initial_global_step = self.global_step.assign(0)
+            sess.run(initial_global_step)
+
+        for epoch in range(start_epoch, start_epoch + num_epochs):
+
+            for step in range(number_of_steps):
+
+                _, _, loss, acc, lr, gs = sess.run(
+                    [self.update_step,
+                     self.increment_global_step,
+                     self.train_loss,
+                     self.accuracy,
+                     self.optimizer._lr_t,  # _learning_rate,  # .optimizer._lr_t,
+                     self.global_step])
+                print("%d,%d,%d,%d,%.4f,%.4f,%.8f"
+                      % (epoch,
+                         self.options['num_epochs'],
+                         step,
+                         self.number_of_steps_per_epoch,
+                         loss, acc, lr))
+
+                if self.options['save_summaries']:
+                    self.writer.add_summary(summ, gs)
+
+                if (self.train_era_step % self.options['save_steps'] == 0) and self.options['save']:
+                    # print("saving model at global step %d..." % global_step)
+                    self.save_model(sess=sess, save_path=self.options['save_model'] + "_epoch%d_step%d" % (epoch, step))
+                    # print("model saved.")
+
+                self.train_era_step += 1
+
+        # save before closing
+        if self.options['save']:
+            self.save_model(sess=sess, save_path=self.options['save_model'] + "_final")
 
     def build_inference_graph(self):
         pass
